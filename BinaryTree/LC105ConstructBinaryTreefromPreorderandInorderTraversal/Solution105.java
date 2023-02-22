751469b2f11ad4bc97c1c17ba0b3d0da8dbc5380
feifan-li <feifanl@usc.edu>
2023-02-22 07:45:06 -0800

    update

--- BinaryTree/LC105ConstructBinaryTreefromPreorderandInorderTraversal/Solution105.java
+++ BinaryTree/LC105ConstructBinaryTreefromPreorderandInorderTraversal/Solution105.java
@@ -1,5 +1,7 @@
 package BinaryTree.LC105ConstructBinaryTreefromPreorderandInorderTraversal;
 
+import java.util.HashMap;
+
 public class Solution105 {
     public static class TreeNode {
         int val;
@@ -12,6 +14,19 @@ public class Solution105 {
     }
     //preorder and inorder consist of unique values.
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-
+        HashMap<Integer,Integer> inMap = new HashMap<>();//num to index
+        for(int i=0;i<inorder.length;++i){
+            inMap.put(inorder[i],i);
+        }
+        return helper(preorder,inorder,0,preorder.length-1,0,inorder.length-1,inMap);
+    }
+    private TreeNode helper(int[] preorder,int[] inorder,int pl,int pr,int il,int ir,HashMap<Integer,Integer> inMap){
+        if(pl>pr || il>ir) return null;
+        TreeNode root = new TreeNode(preorder[pl]);
+        if(pl==pr) return root;
+        int rootIndexInorder = inMap.get(preorder[pl]);
+        root.left = helper(preorder,inorder,pl+1,pl+rootIndexInorder-il,il,rootIndexInorder-1,inMap);
+        root.right = helper(preorder,inorder,pl+rootIndexInorder-il+1,pr,rootIndexInorder+1,ir,inMap);
+        return root;
     }
 }
