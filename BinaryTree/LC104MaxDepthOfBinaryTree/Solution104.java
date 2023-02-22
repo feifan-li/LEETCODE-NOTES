d6ff31e3c2bb9a588666649f667ee156d8867169
feifan-li <feifanl@usc.edu>
2023-02-22 05:53:35 -0800

    update

---
+++ BinaryTree/LC104MaxDepthOfBinaryTree/Solution104.java
@@ -0,0 +1,12 @@
+package BinaryTree.LC104MaxDepthOfBinaryTree;
+
+public class Solution104 {
+    public static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+    }
+    public int maxDepth(TreeNode root) {
+        return root==null ? 0 : Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    }
+}
