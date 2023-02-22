5ac986bae852deb5c5e36076355f18ca9906c6f7
feifan-li <feifanl@usc.edu>
2023-02-22 05:33:24 -0800

    update

---
+++ BinaryTree/LC100SameTree/Solution100.java
@@ -0,0 +1,21 @@
+package BinaryTree.LC100SameTree;
+
+public class Solution100 {
+    public static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        else if(p!=null && q==null) return false;
+        else if(p==null && q!=null) return false;
+        else if(p.val == q.val){
+            boolean leftSame = isSameTree(p.left,q.left);
+            boolean rightSame = isSameTree(p.right,q.right);
+            return leftSame && rightSame;
+        }else{
+            return false;
+        }
+    }
+}
