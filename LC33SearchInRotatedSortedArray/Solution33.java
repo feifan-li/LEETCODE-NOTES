559adc896829dc750654fb9f23568da98d8f3409
feifan-li <feifanl@usc.edu>
2023-02-27 23:32:46 -0800

    lc33

---
+++ LC33SearchInRotatedSortedArray/Solution33.java
@@ -0,0 +1,34 @@
+package LC33SearchInRotatedSortedArray;
+
+public class Solution33 {
+    //must be O(log(n))
+    public static int search(int[] nums, int target) {
+        int n = nums.length;
+        if(n==1) return nums[0]==target?0:-1;
+        int left = 0;
+        int right = n-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && nums[mid]>target){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }
+            if(nums[mid]<=nums[right]){
+                if(nums[mid]<target && target<=nums[right]){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{4,5,6,7,0,1,2},0));
+    }
+}
