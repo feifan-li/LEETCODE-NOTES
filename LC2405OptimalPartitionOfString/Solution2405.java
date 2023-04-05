8480f759866db315b990ca8b32de733ab2488bcb
feifan-li <feifanl@usc.edu>
2023-04-05 11:25:38 -0700

    lc2405

---
+++ LC2405OptimalPartitionOfString/Solution2405.java
@@ -0,0 +1,20 @@
+package LC2405OptimalPartitionOfString;
+
+import java.util.Arrays;
+
+public class Solution2405 {
+    public int partitionString(String s) {
+        //Greedy
+        int[] lastSeen = new int[26];//for each char, record the index it last appears at
+        int ans = 1,subStart = 0;
+        Arrays.fill(lastSeen,-1);
+        for(int i=0;i<s.length();++i){
+            if(lastSeen[s.charAt(i)-'a']>=subStart){
+                ans++;
+                subStart = i;
+            }
+            lastSeen[s.charAt(i)-'a']=i;
+        }
+        return ans;
+    }
+}
