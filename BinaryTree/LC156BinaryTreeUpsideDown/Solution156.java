d9861a553547ca58c26d7a3d5c6a2c744a9501f7
feifan-li <feifanl@usc.edu>
2023-05-16 20:16:26 -0700

    lc156

---
+++ BinaryTree/LC156BinaryTreeUpsideDown/Solution156.java
@@ -0,0 +1,29 @@
+package BinaryTree.LC156BinaryTreeUpsideDown;
+
+public class Solution156 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root==null || (root.left==null && root.right==null)){
+            return root;
+        }
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        TreeNode rootLeft = root.left;
+        TreeNode rootRight = root.right;
+        root.left = null;
+        root.right = null;
+        rootLeft.left = rootRight;
+        rootLeft.right = root;
+        return newRoot;
+    }
+}
