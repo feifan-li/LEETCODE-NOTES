6db3707dea4595b0eb5d93cb0958d430103a5902
feifan-li <feifanl@usc.edu>
2023-05-16 20:42:08 -0700

    lc157

--- LC157ReadNCharsGivenRead4/Reader4.java
+++ LC157ReadNCharsGivenRead4/Reader4.java
@@ -2,6 +2,6 @@ package LC157ReadNCharsGivenRead4;
 
 public class Reader4 {
     public int read4(char[] buf4){
-        return 0;
+        return 0;//read from file and return the actually number of chars it successfully read out to buf4
     }
 }
