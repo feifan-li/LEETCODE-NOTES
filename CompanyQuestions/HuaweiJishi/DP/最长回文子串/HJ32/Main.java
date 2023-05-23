2e7c1325f12e9537782eb8f489554971c85944c3
feifan-li <feifanl@usc.edu>
2023-05-23 01:36:02 -0700

    HJ32

---
+++ CompanyQuestions/HuaweiJishi/DP/最长回文子串/HJ32/Main.java
@@ -0,0 +1,27 @@
+package CompanyQuestions.HuaweiJishi.DP.最长回文子串.HJ32;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        //最长回文子串
+        char[] arr = new char[2*s.length()+1];
+        for(int i=0;i<arr.length;++i){
+            if(i%2==0) arr[i] = '*';
+            else arr[i] = s.charAt(i/2);
+        }
+        int dp = 1;
+        for(int i=1;i<arr.length-1;++i){
+            int left = i-1;
+            int right = i+1;
+            while(left>=0 && right<arr.length && arr[left]==arr[right]){
+                left--;
+                right++;
+            }
+            dp = Math.max(dp,right-left-1);
+        }
+        System.out.println(dp/2);
+    }
+}
