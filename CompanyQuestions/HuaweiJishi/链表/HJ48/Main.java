c328051bce7fb20e2c0c3a51b6e6e15e1d673f8c
feifan-li <feifanl@usc.edu>
2023-05-24 17:31:34 -0700

    HJ48

---
+++ CompanyQuestions/HuaweiJishi/链表/HJ48/Main.java
@@ -0,0 +1,34 @@
+package CompanyQuestions.HuaweiJishi.链表.HJ48;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = in.nextInt();
+        int headVal = in.nextInt();
+        list.add(headVal);
+        for(int i=0;i<n-1;++i){//插入n-1次
+            int newVal = in.nextInt();
+            int afterVal = in.nextInt();
+            if(afterVal==headVal){
+                list.add(1,newVal);
+            }else{
+                for(int j=0;j<list.size();++j){
+                    if(list.get(j)==afterVal){
+                        list.add(j+1,newVal);
+                        break;
+                    }
+                }
+            }
+        }
+        assert list.size()==n;
+        int deleteVal = in.nextInt();
+        for(int i=0;i<n;++i){
+            if(list.get(i)!=deleteVal)  System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+    }
+}
