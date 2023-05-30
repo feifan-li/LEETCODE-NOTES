35f0f0d5e7ac15d61fae2eab249efbeaf100fa50
feifan-li <feifanl@usc.edu>
2023-05-30 07:19:23 -0700

    HJ100

---
+++ CompanyQuestions/HuaweiJishi/HJ100等差数列/Main.java
@@ -0,0 +1,11 @@
+package CompanyQuestions.HuaweiJishi.HJ100等差数列;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(2*n+3*n*(n-1)/2);
+    }
+}
