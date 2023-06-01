1734da2a63336c95b7ef73463264b38c21ce4ad0
feifan-li <feifanl@usc.edu>
2023-06-01 01:35:25 -0700

    lc1872

---
+++ LC_Stone_Game_Series/LC1872StoneGameVIII/Solution1872.java
@@ -0,0 +1,16 @@
+package LC_Stone_Game_Series.LC1872StoneGameVIII;
+
+import java.util.Arrays;
+
+public class Solution1872 {
+    public int stoneGameVIII(int[] stones) {
+        int n = stones.length;
+        int[] sum = new int[n];
+        sum[0] = stones[0];
+        for(int i=1;i<n;++i)    sum[i] = stones[i]+sum[i-1];
+        int[] dp = new int[n];//dp[i]: max difference of current player if remove all stones with index <=k, k>=i
+        dp[n-1] = sum[n-1];//the player can only remove all stones
+        for(int i=n-2;i>0;--i)  dp[i] = Math.max(dp[i+1],sum[i]-dp[i+1]);//choose k>i or k=i
+        return dp[1];
+    }
+}
