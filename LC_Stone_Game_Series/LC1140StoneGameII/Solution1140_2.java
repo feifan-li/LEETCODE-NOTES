1da6373e4036f4610f79966d765d02dcf81f0f92
feifan-li <feifanl@usc.edu>
2023-05-31 23:55:09 -0700

    lc1140_2

---
+++ LC_Stone_Game_Series/LC1140StoneGameII/Solution1140_2.java
@@ -0,0 +1,25 @@
+package LC_Stone_Game_Series.LC1140StoneGameII;
+
+import java.util.Arrays;
+
+public class Solution1140_2 {
+    public int stoneGameII(int[] piles) {
+        int[][] dp = new int[piles.length+1][piles.length+1];
+        int sum = 0;
+        for(int i=0;i<=piles.length;++i){
+            Arrays.fill(dp[i],Integer.MIN_VALUE);
+            if(i<piles.length)  sum += piles[i];
+        }
+        return (f(piles,dp,0,1)+sum)/2;
+    }
+    private int f(int[] piles,int[][] dp,int i,int M){
+        if(i>=piles.length) return 0;
+        if(dp[i][M]!=Integer.MIN_VALUE)  return dp[i][M];
+        int stones = 0;
+        for(int X = 1;X<=Math.min(2*M,piles.length-i);++X){
+            stones += piles[i+X-1];
+            dp[i][M] = Math.max(dp[i][M],stones-f(piles,dp,i+X,Math.max(M,X)));
+        }
+        return dp[i][M];
+    }
+}
