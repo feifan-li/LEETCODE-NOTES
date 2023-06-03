95ca6eb331e9d2fa0a691b43e029b3089941d7f1
feifan-li <feifanl@usc.edu>
2023-06-02 19:51:10 -0700

    lc217

---
+++ LC217ContainsDuplicate/Solution217.java
@@ -0,0 +1,13 @@
+package LC217ContainsDuplicate;
+
+import java.util.Arrays;
+
+public class Solution217 {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;++i){
+            if(nums[i]==nums[i-1])  return true;
+        }
+        return false;
+    }
+}
