2ed665532dd56c99086c92b7e71d81081d3fb668
feifan-li <feifanl@usc.edu>
2023-06-16 22:38:27 -0700

    lc1187

--- Greedy/LC1187MakeArrayStrictlyIncreasing/Solution1187.java
+++ Greedy/LC1187MakeArrayStrictlyIncreasing/Solution1187.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 import java.util.HashMap;
 import javafx.util.Pair;
 public class Solution1187 {
-    public HashMap<Pair,Integer> memo = new HashMap<>();
-    public int makeArrayIncreasing(int[] arr1, int[] arr2) {
+    public static HashMap<Pair<Integer,Integer>,Integer> memo = new HashMap<>();
+    public static int makeArrayIncreasing(int[] arr1, int[] arr2) {
         Arrays.sort(arr2);
         int ans = dp(0,-1,arr1,arr2);
         return ans<=arr1.length?ans:-1;
     }
-    public int dp(int start,int prev,int[] arr1,int[] arr2){
+    public static int dp(int start,int prev,int[] arr1,int[] arr2){
         if(start==arr1.length)  return 0;
         if(memo.containsKey(new Pair<>(start,prev)))    return memo.get(new Pair<>(start,prev));
         int cost = 9999;
@@ -23,7 +23,7 @@ public class Solution1187 {
         memo.put(new Pair<>(start,prev),cost);
         return cost;
     }
-    public int searchFirstIdGreaterThan(int[] arr,int val){
+    public static int searchFirstIdGreaterThan(int[] arr,int val){
         int left = 0, right = arr.length-1;
         while(left<=right){
             int mid = left + (right-left)/2;
@@ -31,5 +31,9 @@ public class Solution1187 {
             else    right = mid-1;
         }
         return left;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makeArrayIncreasing(new int[]{1,5,3,6,7},new int[]{4,3,1}));
     }
 }
