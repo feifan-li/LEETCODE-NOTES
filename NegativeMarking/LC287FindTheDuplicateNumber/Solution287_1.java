43083bf5de81df8d0a1505eff77b56468f88730d
feifan-li <feifanl@usc.edu>
2023-06-25 00:34:24 -0700

    lc287

---
+++ NegativeMarking/LC287FindTheDuplicateNumber/Solution287_1.java
@@ -0,0 +1,20 @@
+package NegativeMarking.LC287FindTheDuplicateNumber;
+
+public class Solution287_1 {
+    public int findDuplicate(int[] nums) {
+        /*Time Complexity: O(n*logn)*/
+        /*Space Complexity: O(1)*/
+        int n = nums.length-1;
+        int min = 1,max = n;
+        while(min<=max){
+            int mid = min+(max-min)/2;
+            int count = 0;
+            for(int num:nums){
+                if(num<=mid)    ++count;
+            }
+            if(count>mid) max = mid-1;
+            else min = mid+1;
+        }
+        return min;
+    }
+}
---
+++ NegativeMarking/LC287FindTheDuplicateNumber/Solution287_2.java
@@ -0,0 +1,21 @@
+package NegativeMarking.LC287FindTheDuplicateNumber;
+
+public class Solution287_2 {
+    /*Negative Marking*/
+    /*Time Compexity: O(n)*/
+    /*Space Complexity: O(1)*/
+    public int findDuplicate(int[] nums) {
+        int dup = -1;
+        for(int num:nums){
+            if(nums[Math.abs(num)]<0){
+                dup = Math.abs(num);
+                break;
+            }
+            nums[Math.abs(num)] *= -1;
+        }
+        for(int i=0;i<nums.length;++i){
+            nums[i] = Math.abs(nums[i]);
+        }
+        return dup;
+    }
+}
