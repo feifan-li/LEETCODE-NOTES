798ac21a8a3e041d1a99cc68ffde41b65a18f1f9
feifan-li <feifanl@usc.edu>
2023-06-26 23:10:05 -0700

    lc298

--- BinaryTree/LC298BinaryTreeLongestConsecutiveSequence/Solution298.java
+++ BinaryTree/LC298BinaryTreeLongestConsecutiveSequence/Solution298.java
@@ -13,25 +13,18 @@ public class Solution298 {
             this.right = right;
         }
     }
+    public int res;
     public int longestConsecutive(TreeNode root) {
-        return helper(root)[0];
+        helper(root,0,root.val);
+        return res;
     }
-    public int[] helper(TreeNode root){
-        /* Return the longest consecutive path's length, and whether this path starts with root(0 No, 1 Yes)*/
-        if(root==null)  return new int[]{0,0};
-        if(root.left==null && root.right==null) return new int[]{1,1};
-        int[] cur = new int[]{1,1};
-        int[] left = helper(root.left);
-        int[] right = helper(root.right);
-        if(root.left!=null && root.val==root.left.val-1){
-            cur[0] = Math.max(cur[0],(left[1]==1?left[0]+1:2));
-            cur[1] = 1;
-        }
-        if(root.right!=null && root.val==root.right.val-1){
-            cur[0] = Math.max(cur[0],(right[1]==1?right[0]+1:2));
-            cur[1] = 1;
-        }
-        if(cur[0]>=Math.max(left[0],right[0]))  return cur;
-        return new int[]{Math.max(left[0],right[0]),0};
+    public void helper(TreeNode root,int max,int target){
+        if(root==null) return;
+        if(root.val==target)    max++;
+        else    max=1;
+        res = Math.max(res,max);
+        helper(root.left,max,root.val+1);
+        helper(root.right,max,root.val+1);
     }
+
 }
