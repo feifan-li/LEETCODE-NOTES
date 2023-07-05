eff4d742a43676b91345dc8654dbfc5489ac83d5
feifan-li <feifanl@usc.edu>
2023-07-05 08:53:35 -0700

    LC324

---
+++ LC324WiggleSortII/Solution324.java
@@ -0,0 +1,16 @@
+package LC324WiggleSortII;
+
+import java.util.Arrays;
+
+public class Solution324 {
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
+        int[] tmp = new int[nums.length];
+        int odd = (nums.length - 1)/2, even = nums.length-1;
+        for(int i=0;i<nums.length;++i){
+            if(i%2==0)  tmp[i] = nums[odd--];
+            else tmp[i] = nums[even--];
+        }
+        System.arraycopy(tmp,0,nums,0,nums.length);
+    }
+}
