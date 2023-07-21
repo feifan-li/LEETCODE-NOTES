b43b4588792d3aa7a639e4a2e086d666b2e300f7
feifan-li <feifanl@usc.edu>
2023-07-20 18:15:01 -0700

    lc37 - Sudoku Solver

--- Backtrack/LC37SudokuSolver/Solution37.java
+++ Backtrack/LC37SudokuSolver/Solution37.java
@@ -2,26 +2,26 @@ package Backtrack.LC37SudokuSolver;
 
 public class Solution37 {
     public void solveSudoku(char[][] board) {
-        boolean res = backtrack(board,0,0);
+        backtrack(board,0,0);
     }
-    private boolean backtrack(char[][] board,int i,int j){
-        int m=9,n=9;
-        if(j==n) return backtrack(board,i+1,0);
-        if(i==m) return true;
-        if(board[i][j]!='.') return backtrack(board,i,j+1);
-        for(char ch='1';ch<='9';ch++){
-            if(!isValid(board,i,j,ch)) continue;
-            board[i][j] = ch;
-            if(backtrack(board,i,j+1)) return true;
-            board[i][j] = '.';
+    public boolean backtrack(char[][] board,int row,int col){
+        if(col==9)  return backtrack(board,row+1,0);
+        if(row==9)  return true;
+        if(board[row][col] != '.')  return backtrack(board,row,col+1);
+        for(char i='1';i<='9';++i){
+            if(isValid(board,row,col,i)){
+                board[row][col] = i;
+                if(backtrack(board,row,col+1))  return true;
+                board[row][col] = '.';
+            }
         }
         return false;
     }
-    private boolean isValid(char[][] board,int row,int col,char n){
+    public boolean isValid(char[][] board,int row, int col,char num){
         for(int i=0;i<9;++i){
-            if(board[row][i]==n) return false;
-            if(board[i][col]==n) return false;
-            if(board[(row/3)*3+i/3][(col/3)*3+i%3]==n) return false;
+            if(board[i][col]==num)  return false;
+            if(board[row][i]==num)  return false;
+            if(board[(row/3)*3+i/3][(col/3)*3+i%3]==num)    return false;
         }
         return true;
     }
