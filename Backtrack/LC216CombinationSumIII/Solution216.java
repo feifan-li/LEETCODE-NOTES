4b745fae4a434e2103d9ebf77cf29cf5fe9b3022
feifan-li <feifanl@usc.edu>
2023-07-20 17:35:59 -0700

    lc216

--- Backtrack/LC216CombinationSumIII/Solution216.java
+++ Backtrack/LC216CombinationSumIII/Solution216.java
@@ -4,20 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution216 {
+    public List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> ans = new ArrayList<>();
-        backtrack(ans,new ArrayList<Integer>(),0,k,n);
-        return ans;
+        int[] nums = new int[]{1,2,3,4,5,6,7,8,9};
+        if(n>45 || n<3 || (k==9 && n!=45))    return res;
+        backtrack(nums,k,n,new ArrayList<Integer>(),0);
+        return res;
     }
-    public void backtrack(List<List<Integer>> ans,ArrayList<Integer> list,int sum,int k,int n){
-        if(list.size()==k && sum==n){
-            ans.add(new ArrayList<>(list));
+    public void backtrack(int[] nums,int targetLen, int target, ArrayList<Integer> list,int start){
+        if(target==0 && targetLen==list.size()){
+            res.add(new ArrayList<>(list));
+            return;
+        }else if(target<0 || list.size()>=targetLen){
             return;
         }
-        if(list.size()==k)  return;
-        for(int i=list.size()==0?1:list.get(list.size()-1)+1;i<=9;++i){
-            list.add(i);
-            backtrack(ans,list,sum+i,k,n);
+        for(int i=start;i<nums.length;++i){
+            list.add(nums[i]);
+            backtrack(nums,targetLen,target-nums[i],list,i+1);
             list.remove(list.size()-1);
         }
     }
