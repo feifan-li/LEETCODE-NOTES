4e824d56bae0ac244dbf3da3fcd0b888efa821b4
feifan-li <feifanl@usc.edu>
2023-07-22 02:45:58 -0700

    lc716

---
+++ OODesign/LC716MaxStack/MaxStack.java
@@ -0,0 +1,40 @@
+package OODesign.LC716MaxStack;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+class MaxStack {
+    public TreeSet<int[]> sorted;
+    public TreeMap<Integer,Integer> stack;
+    public int cnt = 0;
+    public MaxStack() {
+        this.stack = new TreeMap<>();
+        this.sorted = new TreeSet<>((a,b)->(a[0]==b[0]?a[1]-b[1]:a[0]-b[0]));
+    }
+
+    public void push(int x) {
+        stack.put(cnt,x);
+        sorted.add(new int[]{x,cnt});
+        cnt++;
+    }
+
+    public int pop() {
+        int x = stack.get(stack.lastKey());
+        sorted.remove(new int[]{x,stack.lastKey()});
+        return stack.remove(stack.lastKey());
+    }
+
+    public int top() {
+        return stack.get(stack.lastKey());
+    }
+
+    public int peekMax() {
+        return sorted.last()[0];
+    }
+
+    public int popMax() {
+        int[] x = sorted.pollLast();
+        stack.remove(x[1]);
+        return x[0];
+    }
+}
