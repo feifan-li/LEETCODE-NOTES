6510a03d5f7a5a165bc1c7db761332bc1d0a2846
feifan-li <feifanl@usc.edu>
2023-07-22 18:34:17 -0700

    lc370

---
+++ DynamicProgramming/LC370RangeAddition/Solution370.java
@@ -0,0 +1,13 @@
+package DynamicProgramming.LC370RangeAddition;
+
+public class Solution370 {
+    public int[] getModifiedArray(int length, int[][] updates) {
+        int[] res = new int[length];
+        for(int[] u:updates){
+            res[u[0]] += u[2];
+            if(u[1]+1<length)   res[u[1]+1] -= u[2];
+        }
+        for(int i=1;i<length;++i)   res[i] += res[i-1];
+        return res;
+    }
+}
