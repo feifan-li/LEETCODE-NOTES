f39c7ea4424c57d578c83740055a87b93fa05314
feifan-li <feifanl@usc.edu>
2023-08-02 16:36:07 -0700

    lc503

--- MonotonicStack/LC503NextGreaterElementII/Solution503.java
+++ MonotonicStack/LC503NextGreaterElementII/Solution503.java
@@ -4,7 +4,7 @@ public class Solution503 {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length, top = -1;
         int[] ans = new int[n];
-        int[] s = new int[2*n];
+        int[] s = new int[2*n];//monotonic decreasing stack
         for(int i=2*n-1;i>=0;--i){
             while(top>=0 && s[top]<=nums[i%n])  top--;
             ans[i%n] = top==-1?-1:s[top];
