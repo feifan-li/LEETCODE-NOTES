d429a2706a372e02493920329e6052ad673ae963
feifan-li <feifanl@usc.edu>
2023-08-02 16:32:57 -0700

    lc503

---
+++ MonotonicStack/LC503NextGreaterElementII/Solution503.java
@@ -0,0 +1,15 @@
+package MonotonicStack.LC503NextGreaterElementII;
+
+public class Solution503 {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length, top = -1;
+        int[] ans = new int[n];
+        int[] s = new int[2*n];
+        for(int i=2*n-1;i>=0;--i){
+            while(top>=0 && s[top]<=nums[i%n])  top--;
+            ans[i%n] = top==-1?-1:s[top];
+            s[++top] = nums[i%n];
+        }
+        return ans;
+    }
+}
