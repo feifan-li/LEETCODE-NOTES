6775ad9904d35d868a0990474639c45319f31f8f
feifan-li <feifanl@usc.edu>
2023-08-01 17:59:30 -0700

    lc46

---
+++ Backtrack/LC46Permutations/Solution46.java
@@ -0,0 +1,25 @@
+package Backtrack.LC46Permutations;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution46 {
+    List<List<Integer>> ans = new LinkedList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        LinkedList<Integer> track = new LinkedList<>();
+        backtrack(nums,track);
+        return ans;
+    }
+    private void backtrack(int[] nums,LinkedList<Integer> track){
+        if(track.size() == nums.length){
+            ans.add(new LinkedList(track));
+            return;
+        }
+        for(int i=0;i<nums.length;++i){
+            if(track.contains(nums[i])) continue;
+            track.add(nums[i]);
+            backtrack(nums,track);
+            track.removeLast();
+        }
+    }
+}
