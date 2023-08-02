4bff6999f83e63235bb33a0426a3a1b94e76ca78
feifan-li <feifanl@usc.edu>
2023-08-01 20:08:36 -0700

    lc1249

--- Stack/LC1249MinRemoveToMakeValidParentheses/Solution1249.java
+++ Stack/LC1249MinRemoveToMakeValidParentheses/Solution1249.java
@@ -1,4 +1,4 @@
-package LC1249MinRemoveToMakeValidParentheses;
+package Stack.LC1249MinRemoveToMakeValidParentheses;
 
 public class Solution1249 {
     public String minRemoveToMakeValid(String s) {
@@ -6,14 +6,15 @@ public class Solution1249 {
         int[] leftP = new int[s.length()];//position of '(' in sb
         int top = -1;
         for(int i=0;i<s.length();++i){
-            if(s.charAt(i)==')' && top>=0){
-                top--;
+            if(s.charAt(i)==')' && top>=0){//extra ')' will not be added to sb
+                top--;//pop a '('
                 sb.append(')');
             }else if(s.charAt(i)=='('){
                 sb.append('(');
                 leftP[++top] = sb.length()-1;
             }else if(s.charAt(i)!=')')  sb.append(s.charAt(i));
         }
+        //remove extra '(' from sb
         while(top>=0)     sb.replace(leftP[top],leftP[top--]+1,"");
         return sb.toString();
     }
