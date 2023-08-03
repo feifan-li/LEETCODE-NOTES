e6b45d7cedc2c29e6deb68fc37524359cd49eaad
feifan-li <feifanl@usc.edu>
2023-08-02 17:39:59 -0700

    lc84

--- MonotonicStack/LC84LargestRectangleInHistogram/Solution84.java
+++ MonotonicStack/LC84LargestRectangleInHistogram/Solution84.java
@@ -5,20 +5,20 @@ import java.util.Stack;
 public class Solution84 {
     public int largestRectangleArea(int[] h) {
         if(h.length==1) return h[0];
-        int max = 0;
-        Stack<Integer> s = new Stack<Integer>();//mono-stack
+        int max = 0, top = -1;
+        int[] s = new int[h.length];//mono-stack
         for(int i=0;i<h.length;++i){
-            while(!s.isEmpty() && h[i]<=h[s.peek()]){
-                int j = s.pop();
-                int leftBound = s.isEmpty()?-1:s.peek();
+            while(top>=0 && h[i]<=h[s[top]]){
+                int j = s[top--];
+                int leftBound = top==-1?-1:s[top];
                 int cur = h[j]*(i-leftBound-1);
                 max = Math.max(max,cur);
             }
-            s.push(i);
+            s[++top] = i;
         }
-        while(!s.isEmpty()){
-            int j=s.pop();
-            int leftBound = s.isEmpty()?-1:s.peek();
+        while(top>=0){
+            int j=s[top--];
+            int leftBound = top==-1?-1:s[top];
             int cur = h[j]*(h.length-leftBound-1);
             max = Math.max(max,cur);
         }
