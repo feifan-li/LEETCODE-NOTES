c9276201499f2f1ee4f5dba72ac48fb24450d497
feifan-li <feifanl@usc.edu>
2023-08-03 13:44:47 -0700

    lc253

--- SweepLine/LC253MeetingRoomsII/Solution253.java
+++ SweepLine/LC253MeetingRoomsII/Solution253.java
@@ -1,24 +1,25 @@
 package SweepLine.LC253MeetingRoomsII;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 //Given an array of meeting time intervals where intervals[i] = [starti, endi],
 // return the minimum number of conference rooms required.
 public class Solution253 {
     //sweep line
-    //(number of airplanes in the sky)
-    //min rooms <=> max number of conferences hoding at the same time
+    //min rooms <=> max number of overlapping conferences
     public int minMeetingRooms(int[][] intervals) {
-        ArrayList<int[]> list = new ArrayList<>();
-        for(int[] intv:intervals){
-            list.add(new int[]{intv[0],1});
-            list.add(new int[]{intv[1],-1});
+        int[][] list = new int[intervals.length*2][2];
+        for(int i=0;i<intervals.length;++i){
+            list[i*2][0] = intervals[i][0];
+            list[i*2][1] = 1;
+            list[i*2+1][0] = intervals[i][1];
+            list[i*2+1][1] = -1;
         }
-        Collections.sort(list,(a, b)->a[0]==b[0]?a[1]-b[1]:a[0]-b[0]);
+        Arrays.sort(list,(a, b)->(a[0]==b[0]?a[1]-b[1]:a[0]-b[0]));//同一时刻既有开始又有结束则先结束
         int ans = 0,cnt = 0;
-        for(int i=0;i<list.size();++i){
-            int[] p = list.get(i);
+        for(int[] p:list){
             cnt += p[1];
             ans = Math.max(ans,cnt);
         }
