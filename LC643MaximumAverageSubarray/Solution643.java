7ff06524597cad75386c85b7a06f0b865d7a66cc
feifan-li <feifanl@usc.edu>
2023-08-04 16:54:38 -0700

    lc643

---
+++ LC643MaximumAverageSubarray/Solution643.java
@@ -0,0 +1,15 @@
+package LC643MaximumAverageSubarray;
+
+public class Solution643 {
+    public double findMaxAverage(int[] nums, int k) {
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for(int i=1;i<nums.length;++i)  sum[i] = sum[i-1]+nums[i];
+        int kSumMax = sum[k-1];
+        for(int i=1;i<nums.length-k+1;++i){
+            int cur = sum[i+k-1]-sum[i-1];
+            kSumMax = Math.max(kSumMax,cur);
+        }
+        return (double)kSumMax/(double)k;
+    }
+}
