a6d345996e85d55f8505accd6f06b2a680bbb4b4
feifan-li <feifanl@usc.edu>
2023-08-04 14:38:42 -0700

    lc287

--- BinarySearch/LC287FindTheDuplicateNumber/Solution287_2.java
+++ BinarySearch/LC287FindTheDuplicateNumber/Solution287_2.java
@@ -2,20 +2,17 @@ package BinarySearch.LC287FindTheDuplicateNumber;
 
 public class Solution287_2 {
     /*Negative Marking*/
+    //cannot use XOR since input may be like : [2,2,2,2,2]
     /*Time Compexity: O(n)*/
     /*Space Complexity: O(1)*/
     public int findDuplicate(int[] nums) {
         int dup = -1;
         for(int num:nums){
-            if(nums[Math.abs(num)]<0){
-                dup = Math.abs(num);
-                break;
-            }
+            dup = nums[Math.abs(num)]<0?Math.abs(num):-1;
+            if(dup>0)   break;
             nums[Math.abs(num)] *= -1;
         }
-        for(int i=0;i<nums.length;++i){
-            nums[i] = Math.abs(nums[i]);
-        }
+        for(int i=0;i<nums.length;++i)  nums[i] = Math.abs(nums[i]);
         return dup;
     }
 }
