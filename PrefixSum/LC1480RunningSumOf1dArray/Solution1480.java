e9cc2b474b357b757efe19255bd8184954a0cb1d
feifan-li <feifanl@usc.edu>
2023-08-05 11:30:38 -0700

    lc1480

---
+++ PrefixSum/LC1480RunningSumOf1dArray/Solution1480.java
@@ -0,0 +1,10 @@
+package PrefixSum.LC1480RunningSumOf1dArray;
+
+public class Solution1480 {
+    public int[] runningSum(int[] nums) {
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for(int i=1;i<nums.length;++i)  sum[i] = sum[i-1]+nums[i];
+        return sum;
+    }
+}
