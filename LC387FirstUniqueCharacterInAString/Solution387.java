5ff77d4667056255e1f35d063ccc399e57e5a006
feifan-li <feifanl@usc.edu>
2023-08-18 14:38:39 -0700

    lc387

---
+++ LC387FirstUniqueCharacterInAString/Solution387.java
@@ -0,0 +1,13 @@
+package LC387FirstUniqueCharacterInAString;
+
+public class Solution387 {
+    public int firstUniqChar(String s) {
+        //第一个只出现了一次的字符的下标
+        int[] cnt = new int[26];
+        for(int i=0;i<s.length();++i)   cnt[s.charAt(i)-'a']++;
+        for(int i=0;i<s.length();++i){
+            if(cnt[s.charAt(i)-'a']==1) return i;
+        }
+        return -1;
+    }
+}
