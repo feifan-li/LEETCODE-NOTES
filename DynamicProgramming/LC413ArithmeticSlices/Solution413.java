d4484f786b14375c9be97e90dbb67822046de2b7
feifan-li <feifanl@usc.edu>
2024-01-09 14:16:40 -0800

    dp/lc413

--- DynamicProgramming/LC413ArithmeticSlices/Solution413.java
+++ DynamicProgramming/LC413ArithmeticSlices/Solution413.java
@@ -1,16 +1,15 @@
 package DynamicProgramming.LC413ArithmeticSlices;
 
 public class Solution413 {
-
-        public int numberOfArithmeticSlices(int[] nums) {
-            int n = nums.length, ans = 0;
-            int[][] dp = new int[n][4001];//dp[i][d]: 公差为d，以nums[i]结尾的子数组（>=2个元素）个数
-            for(int i=1;i<n;++i){
-                int d = nums[i]-nums[i-1]+2001;
-                dp[i][d] += dp[i-1][d]+1;
-                ans += dp[i-1][d];
-            }
-            return ans;
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length, ans = 0;
+        int[] d = new int[n];//以nums[i]结尾的子数组（>=2个元素）公差
+        int[] dp = new int[n];//dp[i]: 以nums[i]结尾的子数组（>=2个元素）个数
+        for(int i=1;i<n;++i){
+            d[i] = nums[i]-nums[i-1];
+            dp[i] += (d[i-1]==d[i]?dp[i-1]+1:1);
+            ans += (d[i-1]==d[i]?dp[i-1]:0);
         }
-
+        return ans;
+    }
 }
