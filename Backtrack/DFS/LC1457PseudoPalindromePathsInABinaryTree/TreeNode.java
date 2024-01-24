321f26bfd267c3b2a6e651cc16ff00686d3113b2
feifan-li <feifanl@usc.edu>
2024-01-23 16:48:57 -0800

    lc1457

---
+++ Backtrack/DFS/LC1457PseudoPalindromePathsInABinaryTree/TreeNode.java
@@ -0,0 +1,14 @@
+package Backtrack.DFS.LC1457PseudoPalindromePathsInABinaryTree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
