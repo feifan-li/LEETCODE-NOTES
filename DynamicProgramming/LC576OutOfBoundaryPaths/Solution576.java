4df7400517fe8c1aa99baec0552911cee1a0460e
feifan-li <feifanl@usc.edu>
2024-01-25 18:49:42 -0800

    lc576

--- DynamicProgramming/LC576OutOfBoundaryPaths/Solution576.java
+++ DynamicProgramming/LC576OutOfBoundaryPaths/Solution576.java
@@ -4,7 +4,7 @@ public class Solution576 {
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         if(maxMove==0)  return 0;
         int MOD = 1_000_000_007;
-        long[][][] dp = new long[m+2][n+2][maxMove];
+        long[][][] dp = new long[m+2][n+2][2];
         //base cases:
         for(int j=1;j<=n;++j){
             dp[1][j][0] += 1;
@@ -18,12 +18,12 @@ public class Solution576 {
         for(int x = 1;x < maxMove;++x){
             for(int i=1;i<=m;++i){
                 for(int j=1;j<=n;++j){
-                    dp[i][j][x] += dp[i-1][j][x-1] + dp[i][j-1][x-1]
-                            + dp[i+1][j][x-1] + dp[i][j+1][x-1];
-                    dp[i][j][x] %= MOD;
+                    dp[i][j][x%2] = dp[i-1][j][(x-1)%2] + dp[i][j-1][(x-1)%2]
+                            + dp[i+1][j][(x-1)%2] + dp[i][j+1][(x-1)%2];
+                    dp[i][j][x%2] %= MOD;
                 }
             }
-            ans = (ans + dp[startRow+1][startColumn+1][x])%MOD;
+            ans = (ans + dp[startRow+1][startColumn+1][x%2])%MOD;
         }
         return (int)(ans%MOD);
     }
